995dbc3820928c0002eb009cce0f727a9652a10f
woori <woori@DESKTOP-3T7OJU9>
2023-06-12 17:37:40 +0900

    테스트

---
+++ shop/src/main/java/com/spring/shop/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.spring.shop.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class MemberController {
+
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	//회원가입 페이지 이동
+	@RequestMapping(value = "member/join", method = RequestMethod.GET)
+	public String joinGET() {
+		
+		logger.info("회원가입 페이지 진입");
+		return "member/join";
+	}
+	
+	//로그인 페이지 이동
+	@RequestMapping("member/login")
+	public String loginGET() {
+		
+		logger.info("로그인 페이지 진입");
+		System.out.println("로그인페이지");
+		return "member/login";
+	}
+	
+}
